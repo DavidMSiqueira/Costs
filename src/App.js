26160bf9dcdd672b02e57a6ffa877529defd56f1
David Machado <50080841+DavidMSiqueira@users.noreply.github.com>
2022-04-12 19:00:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
+
+import Container from './components/layout/Container';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+function App() {
+  return (
+
+    <div>
+
+      <header>
+        <Router>
+
+          <Navbar />
+
+          <Container customClass='min-height'>
+            <Routes>
+              <Route path='/' exact='true' element={<Home />} />
+              <Route path='/company' element={<Company />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/newproject' element={<NewProject />} />
+              <Route path='/projects' element={<Projects />} />
+              <Route path='/project/:id' element={<Project />} />
+
+            </Routes>
+          </Container>
+
+          <Footer />
+
+        </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
